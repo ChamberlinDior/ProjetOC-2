2e2cc75da86388c4f3b79ab92a1673a98bf2d2c0
chamberlin dior ebaley <chamberlindior@gmail.com>
2022-11-25 13:47:27 +0100

    champs contenant les données

--- src/com/missionTrois/AnalyticsCounter.java
+++ src/com/missionTrois/AnalyticsCounter.java
@@ -10,16 +10,16 @@ import com.missionTrois.interfaces.ISymptomsCompute;
 import java.util.List;
 import java.util.Map;
 public class AnalyticsCounter {
-
+        // champs contenant les données
     public static String filepath = "symptoms.txt";
     public static String outputFilepath = "result.out";
 
     public static void main(String args[]) throws Exception {
-              //objet ayant pour objectif de lire placer dans une liste
+              //objet ayant pour objectif de lire le fichier  placer dans une liste
         System.out.println("Load symptom file");
         ISymptomReader reader = new ReadSymptomDataFromFile(filepath);
         List<String> symptomList = reader.getSymptoms();
-
+         //objet servant à comptr
         System.out.println("Compute symptom");
         ISymptomsCompute compute = new SymptomsCompute();
         Map<String, Integer> symptomMap = compute.getMapFromList(symptomList);
