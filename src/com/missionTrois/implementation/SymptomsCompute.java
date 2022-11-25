924c43de8f7b088baec423e46210272cc497dc16
chamberlin dior ebaley <chamberlindior@gmail.com>
2022-11-25 14:52:03 +0100

    utilisation de la boucle for

--- src/com/missionTrois/implementation/SymptomsCompute.java
+++ src/com/missionTrois/implementation/SymptomsCompute.java
@@ -7,10 +7,10 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class SymptomsCompute implements ISymptomsCompute {
-
+     // boucler une Map
     public Map<String, Integer> getMapFromList(List<String> symptomList) {
         Map<String, Integer> symptomMap = new TreeMap<>();
-        for (String symptom : symptomList) {
+        for (String symptom : symptomList) {  //utilisation de la boucle 'for'
             Integer currentValue = symptomMap.containsKey(symptom)
                     ? symptomMap.get(symptom) + 1
                     : 1;
