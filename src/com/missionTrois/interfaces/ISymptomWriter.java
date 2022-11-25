1fed97db7055f0b4a7a37d98bc28f6ecbbf1f13f
chamberlin dior ebaley <chamberlindior@gmail.com>
2022-11-25 16:33:39 +0100

    methode write dans l'interface ISymptomsWriter

--- src/com/missionTrois/interfaces/ISymptomWriter.java
+++ src/com/missionTrois/interfaces/ISymptomWriter.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.Map;
 
 public interface ISymptomWriter {
-
+ // method write utilisant l'exception
     void write(Map<String, Integer> map) throws IOException;
 
 
