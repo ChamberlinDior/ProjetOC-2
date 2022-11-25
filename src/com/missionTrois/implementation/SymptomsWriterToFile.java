99e44ef3402a3223f7b481d74b566a8c434d556c
chamberlin dior ebaley <chamberlindior@gmail.com>
2022-11-25 15:12:23 +0100

    fichier de sortie

--- src/com/missionTrois/implementation/SymptomsWriterToFile.java
+++ src/com/missionTrois/implementation/SymptomsWriterToFile.java
@@ -16,7 +16,7 @@ public class SymptomsWriterToFile implements ISymptomWriter {
 
     public void write(Map<String, Integer> map) throws IOException {
         FileWriter writer = new FileWriter(outputFilepath);
-
+       // utilisation de la boucle forEach pour écrire les symptoms dans le fichier de sortie dans l'odre alphabetique
         map.forEach((key, value) -> {
             try {
                 String v = key + " : " +  value;
